9250a434e87db5e92fa620367498a3812607f336
kashiimalik <153726636+kashiihere01@users.noreply.github.com>
2024-05-28 14:41:52 +0500

    box rotation

---
+++ rotate-animation/script.js
@@ -0,0 +1,20 @@
+
+// gsap.to means intial to final
+gsap.to("#box",{
+x:1000,
+duration:2,
+delay:1,
+rotate:360,
+backgroundColor:"red",
+borderRadius:"50%"
+// scale = size
+})
+// gsap.from means Final to Intial
+gsap.from("#box2",{
+    x:1000,
+    duration:2,
+    delay:1,
+    rotate:360,
+backgroundColor:"red",
+borderRadius:"50%"
+    })
